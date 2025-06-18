3abf915af855156e9e478cf7cf1b8d9ab0abb79d
mzubairr <mzubair5979@gmail.com>
2025-06-18 17:51:29 +0500

    project initialize

---
+++ app/_layout.tsx
@@ -0,0 +1,27 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="+not-found" />
+        <StatusBar style="auto" />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.conatiner}>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+  }
+});
